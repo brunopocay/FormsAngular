af01c19cb3bb9ba5a0639b9cb4b94d92f14a1c90
cbm <cbm.info.docs@gmail.com>
2023-06-16 17:06:16 -0300

    APrendendo forms validators em angular

---
+++ src/app/directives/maior-idade.directive.ts
@@ -0,0 +1,28 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+@Directive({
+  selector: '[MaiorIdadeValidator]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: MaiorIdadeDirective,
+    multi: true
+  }]
+})
+export class MaiorIdadeDirective implements Validator {
+
+  constructor() { }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    const datanasc = control.value;
+    const anonasc = new Date(datanasc).getFullYear();
+    const idadeAtual = anonasc + 18;;
+    const atual = new Date().getFullYear();
+
+    const result = idadeAtual <= atual;
+    return result ? null: {'maiorIdadeValidator': true};
+    
+  }
+  
+
+}
